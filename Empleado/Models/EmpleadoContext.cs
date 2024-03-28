8f89de7e2f4b4bc4b4850a9ea0f2223660803cda
puri0074 <1472315@senaati.pe>
2024-03-27 22:22:09 -0500

    Avance de registro de un empleado

---
+++ Empleado/Models/EmpleadoContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Identity.Client;
+
+namespace Empleado.Models
+{
+    public partial class EmpleadoContext : DbContext
+    {
+        public EmpleadoContext()
+        {
+        }
+
+        public EmpleadoContext(DbContextOptions<EmpleadoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuario> Usuarios { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\senati;Database=Emplado;Integrated Security=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.Property(e => e.Clave)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Fecha).HasColumnType("date");
+            });
+        }
+    }
+}
---
+++ Empleado/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace Empleado.Models
+{
+    public class Usuario
+    {
+        public int ID { get; set; }
+        public string? Nombre { get; set; }
+        public DateTime? Fecha { get; set;}
+        public string? Clave { get; set;}
+    }
+}
